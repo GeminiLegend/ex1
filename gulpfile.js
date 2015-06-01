62995d49087d05020a1c3f894346c092b1100ba0
peb7268 <peb7268@gmail.com>
2015-05-31 21:57:36 -0400

    base styling enhancements

--- gulpfile.js
+++ gulpfile.js
@@ -7,9 +7,11 @@ gulp.task('sass', function () {
   gulp.src('./public/sass/**/*.scss')
     .pipe(sass().on('error', sass.logError))
     .pipe(gulp.dest('./public/css'));
+    //.pipe(livereload());
 });
  
 gulp.task('watch', function () {
+  //livereload.listen();
   gulp.watch('./public/sass/**/*.scss', ['sass']);
 });
 
